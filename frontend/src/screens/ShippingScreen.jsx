bc1016af5de07e90f113caf07bc29284ad3b6315
SergiuCrn <jghaba@users.noreply.github.com>
2024-04-22 23:39:05 +0300

    FE: Payment Screen added

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -4,6 +4,7 @@ import { UseDispatch, useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import FormContainer from "../Components/FormContainer";
 import { saveShippingAddress } from "../slices/cartSlice";
+import CheckoutSteps from "../Components/CheckoutSteps";
 
 const ShippingScreen = () => {
   const cart = useSelector((state) => state.cart);
@@ -27,6 +28,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
 
       <Form onSubmit={submitHandler}>
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,9 +1,57 @@
+// import { createSlice } from "@reduxjs/toolkit";
+// import { updateCart } from "../utils/cartUtils";
+
+// const initialState = localStorage.getItem("cart")
+//   ? JSON.parse(localStorage.getItem("cart"))
+//   : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
+
+// const cartSlice = createSlice({
+//   name: "cart",
+//   initialState,
+//   reducers: {
+//     addToCart: (state, action) => {
+//       const item = action.payload;
+//       const existItem = state.cartItems.find((x) => x._id === item._id);
+
+//       if (existItem) {
+//         state.cartItems = state.cartItems.map((x) =>
+//           x._id === existItem._id ? item : x
+//         );
+//       } else {
+//         state.cartItems = [...state.cartItems, item];
+//       }
+
+//       return updateCart(state);
+//     },
+//     removeFromCart: (state, action) => {
+//       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+//       return updateCart(state);
+//     },
+//     saveShippingAddress: (state, action) => {
+//       state.shippingAddress = action.payload;
+//       return updateCart(state);
+//     },
+//     savePaymentMethod: (state, action) => {
+//       state.paymentMethod = action.payload;
+//       return updateCart(state);
+//     },
+//   },
+// });
+
+// export const {
+//   addToCart,
+//   removeFromCart,
+//   saveShippingAddress,
+//   savePaymentMethod,
+// } = cartSlice.actions;
+
 import { createSlice } from "@reduxjs/toolkit";
 import { updateCart } from "../utils/cartUtils";
 
 const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
-  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" }; //implement STRIPE here
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
 const cartSlice = createSlice({
   name: "cart",
@@ -25,17 +73,31 @@ const cartSlice = createSlice({
     },
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
-
       return updateCart(state);
     },
     saveShippingAddress: (state, action) => {
       state.shippingAddress = action.payload;
       return updateCart(state);
     },
+    savePaymentMethod: (state, action) => {
+      const paymentMethod = action.payload;
+
+      if (paymentMethod === "Cash") {
+        state.paymentMethod = "Cash on Delivery";
+      } else {
+        state.paymentMethod = paymentMethod;
+      }
+
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress } =
-  cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
