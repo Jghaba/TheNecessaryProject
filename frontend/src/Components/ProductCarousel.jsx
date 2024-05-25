3371f0f5ef47e81f8bcef960b0bdb6a8e2bc696c
SergiuCrn <jghaba@users.noreply.github.com>
2024-05-25 18:37:57 +0300

    updated items list

--- frontend/src/Components/ProductCarousel.jsx
+++ frontend/src/Components/ProductCarousel.jsx
@@ -12,7 +12,7 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant="danger">{error}</Message>
   ) : (
-    <Carousel pause="hover" className="bg-primary mb-4">
+    <Carousel pause="hover" className="bg-dark mb-4">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
